9766660de1b6019e8864fc51785b2d347a9f89a1
LupitaMontilla <full.mont.kyoko@gmail.com>
2018-12-23 00:15:53 +0900

    キャラクター一覧表示時と、アイコン表示更新時の一部処理を共通化。極蛮神一覧ページ、ミニオン一覧ページの処理を共通化。

--- js/list_common.js
+++ js/list_common.js
@@ -154,49 +154,10 @@ $(function() {
             var chara_id = $(this).attr('id').split('_')[2];
             var $content = $('#ldst_main_'+chara_id);
 
-            var $minions = $('#minion').children('.items').find('input[type="text"]');
-            var tooltips = new Array();
-            var minions = new Array();
-            $minions.each(function(index){
-                if ($(this).val().indexOf('job:') === 0) {
-                    var job_name;
-                    var job_names = $(this).val().split(':')[1].split(' / ');
-                    var $job_name;
-                    for (var i = 0; i < job_names.length; i++) {
-                        var $job = $content.find('.character__job__name:contains("'+job_names[i]+'")');
-                        if ($job.length) {
-                            job_name = job_names[i];
-                            $job_name = $job;
-                        }
-                    }
-                    var $job_icon = $job_name.siblings('.character__job__icon');
-                    var $job_level = $job_name.siblings('.character__job__level');
-                    tooltips.push(job_name);
-                    minions.push($job_icon);
-                    tooltips.push('');
-                    minions.push($job_level);
-                } else {
-                    tooltips.push($(this).val());
-                    minions.push($content.find('.character__item_icon[data-tooltip="'+$(this).val()+'"]'));
-                }
-            });
+            //var $minions = $('#minion').children('.items').find('input[type="text"]');
 
             var key_number = 4;
-            for (var i = 0; i < minions.length; i++) {
-                var $minion = $(minions[i]).clone();
-                var item_type = 'mount_image icon';
-                if ($minion.attr('class') === 'character__job__icon') {
-                    item_type = 'job_icon icon';
-                }
-                if ($minion.attr('class') === 'character__job__level') {
-                    item_type = 'job_level';
-                }
-                $('.list').append('<li id="mount'+i+'_'+chara_id+'" class="'+item_type+' list_item" data-sortkey="'+(key_number++)+'"></li>');
-                if (tooltips[i] !== '') {
-                    $('#mount'+i+'_'+chara_id).append('<span class="tooltip"><span class="text">'+tooltips[i]+'</span></span>');
-                }
-                $minion.appendTo('#mount'+i+'_'+chara_id);
-			}
+            addListItems($content, chara_id, key_number);
             sortByCharacterName();
         });
     });
@@ -826,4 +787,91 @@ function getMyCharacterData(character_id) {
             $('#freecompany_id').val(freecompany_id);
         }
 	);
+}
+
+function addCharaData(chara_id) {
+	return $.ajax({
+		url: '/php/ldst_access.php?url='+LODESTONE_URL+'/character/'+chara_id,
+		type: 'GET',
+		dataType: 'json',
+		loadingHide: function(res){}
+	})
+	.then(
+		function(res){
+			$content = $(res.data);
+
+			var key_number = 0;
+			$('.list').append('<li id="break_'+chara_id+'" class="break list_item" data-sortkey="'+(key_number++)+'"></li>');
+			$('.list').append('<li id="delete_'+chara_id+'" class="delete list_item" data-sortkey="'+(key_number++)+'"><a href="javascript:void(0);" class="delete_btn button" data-id="'+chara_id+'">一覧から消す</a></li>');
+			$('.list').append('<li id="chara_face_'+chara_id+'" class="chara_face list_item" data-sortkey="'+(key_number++)+'"></li><li id="chara_name_'+chara_id+'" class="chara_name list_item" data-sortkey="'+(key_number++)+'"></li>');
+
+			var user_face_image = $content.find('.frame__chara__face').html();
+			var user_name_text  = $content.find('.frame__chara__name').text();
+			var user_world_text = $content.find('.frame__chara__world').text();
+
+			$('#chara_face_'+chara_id).append(user_face_image);
+			$('#chara_name_'+chara_id).append('<em>'+user_name_text+'</em>');
+			$('#chara_name_'+chara_id).append('<span class="character_world">'+user_world_text+'</span>');
+
+			addListItems($content, chara_id, key_number);
+
+			$content.find('script').remove();
+			var chara_data = '<li id="ldst_main_'+chara_id+'" class="ldst_main" data-sortkey="">'+$content.find('.ldst__main').html()+'</li>';
+			setDeleteButtonRightPosition()
+			addCharacterData(chara_data);
+		},
+		function(XMLHttpRequest, textStatus, errorThrown) {
+			alert('キャラクターの情報が取得できませんでした。');
+			console.log("XMLHttpRequest: " + XMLHttpRequest.status);
+			console.log("textStatus: " + textStatus);
+			console.log("errorThrown: " + errorThrown.message);
+		}
+	);
+}
+
+function addListItems($content, chara_id, key_number) {
+	var $items = $('#minion .items').find('input[readonly="readonly"]');
+	var tooltips = new Array();
+	var minions = new Array();
+	$items.each(function(index){
+		console.log(index);
+		console.log($(this).val());
+		if ($(this).val().indexOf('job:') === 0) {
+			var job_name;
+			var job_names = $(this).val().split(':')[1].split(' / ');
+			var $job_name;
+			for (var i = 0; i < job_names.length; i++) {
+				var $job = $content.find('.character__job__name:contains("'+job_names[i]+'")');
+				if ($job.length) {
+					job_name = job_names[i];
+					$job_name = $job;
+				}
+			}
+			var $job_icon = $job_name.siblings('.character__job__icon');
+			var $job_level = $job_name.siblings('.character__job__level');
+			tooltips.push(job_name);
+			minions.push($job_icon);
+			tooltips.push('');
+			minions.push($job_level);
+		} else {
+			tooltips.push($(this).val());
+			minions.push($content.find('.character__item_icon[data-tooltip="'+$(this).val()+'"]'));
+		}
+	});
+
+    for (var i = 0; i < minions.length; i++) {
+        var $minion = $(minions[i]).clone();
+        var item_type = 'mount_image icon';
+        if ($minion.attr('class') === 'character__job__icon') {
+            item_type = 'job_icon icon';
+        }
+        if ($minion.attr('class') === 'character__job__level') {
+            item_type = 'job_level';
+        }
+        $('.list').append('<li id="mount'+i+'_'+chara_id+'" class="'+item_type+' list_item" data-sortkey="'+(key_number++)+'"></li>');
+        if (tooltips[i] !== '') {
+            $('#mount'+i+'_'+chara_id).append('<span class="tooltip"><span class="text">'+tooltips[i]+'</span></span>');
+        }
+        $minion.appendTo('#mount'+i+'_'+chara_id);
+    }
 }
--- js/minion.js
+++ js/minion.js
@@ -9,22 +9,10 @@ function addCharaData(chara_id) {
 		function(res){
 			$content = $(res.data);
 
-			var target_name = $('#minion').find('input[readonly="readonly"]');
-            var tooltips = new Array();
-
-            var minions = new Array();
-            $(target_name).each(function(index){
-                tooltips.push($(this).val());
-                minions.push($content.find('.character__item_icon[data-tooltip="'+$(this).val()+'"]'));
-            });
-
 			var key_number = 0;
 			$('.list').append('<li id="break_'+chara_id+'" class="break list_item" data-sortkey="'+(key_number++)+'"></li>');
 			$('.list').append('<li id="delete_'+chara_id+'" class="delete list_item" data-sortkey="'+(key_number++)+'"><a href="javascript:void(0);" class="delete_btn button" data-id="'+chara_id+'">一覧から消す</a></li>');
 			$('.list').append('<li id="chara_face_'+chara_id+'" class="chara_face list_item" data-sortkey="'+(key_number++)+'"></li><li id="chara_name_'+chara_id+'" class="chara_name list_item" data-sortkey="'+(key_number++)+'"></li>');
-			for (i = 0; i < minions.length; i++) {
-				$('.list').append('<li id="mount'+i+'_'+chara_id+'" class="mount_image list_item" data-sortkey="'+(key_number++)+'"><span class="tooltip"><span class="text">'+tooltips[i]+'</span></span></li>');
-			}
 
 			var user_face_image = $content.find('.frame__chara__face').html();
 			var user_name_text  = $content.find('.frame__chara__name').text();
@@ -34,10 +22,7 @@ function addCharaData(chara_id) {
 			$('#chara_name_'+chara_id).append('<em>'+user_name_text+'</em>');
 			$('#chara_name_'+chara_id).append('<span class="character_world">'+user_world_text+'</span>');
 
-			for (i = 0; i < minions.length; i++) {
-				var $minion = $(minions[i]).clone();
-				$minion.appendTo('#mount'+i+'_'+chara_id);
-			}
+			addListItems($content, chara_id, key_number);
 
 			$content.find('script').remove();
 			var chara_data = '<li id="ldst_main_'+chara_id+'" class="ldst_main" data-sortkey="">'+$content.find('.ldst__main').html()+'</li>';
@@ -51,4 +36,51 @@ function addCharaData(chara_id) {
 			console.log("errorThrown: " + errorThrown.message);
 		}
 	);
+}
+
+function addListItems($content, chara_id, key_number) {
+	var $items = $('#minion .items').find('input[readonly="readonly"]');
+	var tooltips = new Array();
+	var minions = new Array();
+	$items.each(function(index){
+		console.log(index);
+		console.log($(this).val());
+		if ($(this).val().indexOf('job:') === 0) {
+			var job_name;
+			var job_names = $(this).val().split(':')[1].split(' / ');
+			var $job_name;
+			for (var i = 0; i < job_names.length; i++) {
+				var $job = $content.find('.character__job__name:contains("'+job_names[i]+'")');
+				if ($job.length) {
+					job_name = job_names[i];
+					$job_name = $job;
+				}
+			}
+			var $job_icon = $job_name.siblings('.character__job__icon');
+			var $job_level = $job_name.siblings('.character__job__level');
+			tooltips.push(job_name);
+			minions.push($job_icon);
+			tooltips.push('');
+			minions.push($job_level);
+		} else {
+			tooltips.push($(this).val());
+			minions.push($content.find('.character__item_icon[data-tooltip="'+$(this).val()+'"]'));
+		}
+	});
+
+    for (var i = 0; i < minions.length; i++) {
+        var $minion = $(minions[i]).clone();
+        var item_type = 'mount_image icon';
+        if ($minion.attr('class') === 'character__job__icon') {
+            item_type = 'job_icon icon';
+        }
+        if ($minion.attr('class') === 'character__job__level') {
+            item_type = 'job_level';
+        }
+        $('.list').append('<li id="mount'+i+'_'+chara_id+'" class="'+item_type+' list_item" data-sortkey="'+(key_number++)+'"></li>');
+        if (tooltips[i] !== '') {
+            $('#mount'+i+'_'+chara_id).append('<span class="tooltip"><span class="text">'+tooltips[i]+'</span></span>');
+        }
+        $minion.appendTo('#mount'+i+'_'+chara_id);
+    }
 }
